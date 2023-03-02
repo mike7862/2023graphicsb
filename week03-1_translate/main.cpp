d557df7007c431ca949db6730ff39051d2b2564f
mike7862 <mikelai1012@gmail.com>
2023-03-02 16:49:26 +0800

    week03

---
+++ week03-1_translate/main.cpp
@@ -0,0 +1,19 @@
+#include <GL/glut.h>
+void display()
+{
+    glPushMatrix();///備分矩陣
+        glTranslatef( 0.5, 0.5, 0);
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();///還原矩陣
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+glutCreateWindow("week03");
+glutDisplayFunc(display);
+glutMainLoop();
+}
