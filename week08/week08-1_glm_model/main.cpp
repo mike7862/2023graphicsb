e8a29668f0ff8c3abada2cc36e028b3980a4e0fe
mike7862 <91170810+mike7862@users.noreply.github.com>
2023-04-06 16:50:43 +0800

    Add files via upload

---
+++ week08/week08-1_glm_model/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel=NULL;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    if(pmodel=NULL){
+        pmodel=glmReadOBJ("f-16.obj");///檔名用你的檔名
+        glmUnitize(pmodel);
+        }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+
+    ///glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+glutCreateWindow("week08");
+glutDisplayFunc(display);
+glutMainLoop();
+}
