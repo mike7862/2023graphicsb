d945d2bc60429a3f9fa4bc6b66349e8bf031e175
mike7862 <mikelai1012@gmail.com>
2023-05-18 16:58:09 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,6 +7,7 @@ GLMmodel * rightarm = NULL;
 GLMmodel * righthand = NULL;
 int show[4] = {1,1,1,1};
 int ID = 2;
+int angle = 0;
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
@@ -30,7 +31,7 @@ void display()
     glPushMatrix();
         glScalef(0.3, 0.3, 0.3);
         glPushMatrix();
-            glTranslatef(teapotX, teapotY, 0);
+            //glTranslatef(teapotX, teapotY, 0);
 
             if(ID==0) glColor3f(1,0,0);
             else glColor3f(1,1,1);
@@ -41,14 +42,31 @@ void display()
         else glColor3f(1,1,1);
         if (show[1]) glmDraw(body, GLM_MATERIAL);
 
-        if(ID==2) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if (show[2]) glmDraw(rightarm, GLM_MATERIAL);
+        glPushMatrix();
+            ///glTranslatef(teapotX, teapotY, 0);
+            glTranslatef(+1.300000, +0.540000, 0);
+            glRotatef(angle, 0, 0, 1);
+            glTranslatef(-1.300000, -0.540000, 0);
 
-        if(ID==3) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if (show[3]) glmDraw(righthand, GLM_MATERIAL);
+            if(ID==2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if (show[2]) glmDraw(rightarm, GLM_MATERIAL);
+
+
+            glPushMatrix();
+                ///glTranslatef(teapotX, teapotY, 0);
+                glTranslatef(1.939999, 0.020000, 0);
+                glRotatef(angle, 0, 0, 1);
+                glTranslatef(-1.939999, -0.020000, 0);
+                if(ID==3) glColor3f(1,0,0);
+                else glColor3f(1,1,1);
+                if (show[3]) glmDraw(righthand, GLM_MATERIAL);
+            glPopMatrix();
+        glPopMatrix();
+
     glPopMatrix();
+    glColor3f(0,1,0);
+    glutSolidTeapot( 0.02 );
     glutSwapBuffers();
 }
 int oldX=0, oldY=0;
@@ -60,8 +78,10 @@ void  mouse(int button,int state,int x,int y)
     }
 }
 void motion(int x, int y){
-    teapotX += (x - oldX)/150.0;
-    teapotY -= (y - oldY)/150.0;
+    teapotX += (x - oldX)/150.0*3;
+    teapotY -= (y - oldY)/150.0*3;
+    printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
+    angle += x-oldX;
     oldX = x;
     oldY = y;
     glutPostRedisplay();
@@ -70,7 +90,7 @@ int main(int argc,char** argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
-    glutCreateWindow("week13");
+    glutCreateWindow("week14");
 
     glutMotionFunc(motion);
     glutDisplayFunc(display);
